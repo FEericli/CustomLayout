c7efaff3f0d279529a3417b579d1dbf13e4176d0
FEericli <deltalo2000@gmail.com>
2015-07-18 02:27:41 +0800

    20150718

--- app/src/main/java/demo/ma390104/customlayout/Main.java
+++ app/src/main/java/demo/ma390104/customlayout/Main.java
@@ -4,34 +4,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Main extends AppCompatActivity {
+
+    private ListView cListView;
+    private ArrayList<Item> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadData();
+        findViews();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void findViews(){
+        cListView = (ListView)findViewById(R.id.lsvCList);
+        ItemAdapter adapter = new ItemAdapter(this.getApplicationContext(), data);
+        cListView.setAdapter(adapter);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void loadData(){
+        data = new ArrayList<Item>();
+        data.add(new Item("第1個Item", R.mipmap.ic_launcher));
+        data.add(new Item("第2個Item", R.mipmap.ic_launcher));
+        data.add(new Item("第3個Item", R.mipmap.ic_launcher));
+        data.add(new Item("第4個Item", R.mipmap.ic_launcher));
+        data.add(new Item("第5個Item", R.mipmap.ic_launcher));
+        data.add(new Item("第6個Item", R.mipmap.ic_launcher));
+        data.add(new Item("第7個Item", R.mipmap.ic_launcher));
+        data.add(new Item("第8個Item", R.mipmap.ic_launcher));
+        data.add(new Item("第9個Item", R.mipmap.ic_launcher));
+        data.add(new Item("第10個Item", R.mipmap.ic_launcher));
     }
+
 }
